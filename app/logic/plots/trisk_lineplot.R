dd1c10f2a39a5fc2cc894a2e6d5e03b04543b5c5
bertrand <bertrand.gallice.pro@gmail.com>
2024-02-24 16:26:01 +0100

    new lineplots

---
+++ app/logic/plots/trisk_lineplot.R
@@ -0,0 +1,81 @@
+box::use(
+  ggplot2[ggplot, aes],
+  ggrepel[geom_text_repel]
+)
+
+
+pipeline_trisk_line_plot <- function(
+    trajectories_data,
+    x_var = "year",
+    facet_var = "ald_business_unit",
+    linecolor = "ald_sector") {
+  linecolor <- dplyr::intersect(colnames(trajectories_data), linecolor)
+
+  data_trisk_line_plot <- prepare_for_trisk_line_plot(
+    trajectories_data=trajectories_data, 
+    facet_var=facet_var, 
+    linecolor=linecolor)
+
+  trisk_line_plot <- draw_trisk_line_plot(
+    data_trisk_line_plot,
+    x_var = x_var, 
+    facet_var = facet_var, 
+    linecolor = linecolor
+  )
+
+  return(trisk_line_plot)
+}
+
+prepare_for_trisk_line_plot <- function(trajectories_data, facet_var, linecolor) {
+  # Preprocessing to calculate percentages of the maximum value
+data_trisk_line_plot <- trajectories_data |>
+    tidyr::pivot_longer(
+        cols = dplyr::starts_with("production_"),
+        names_to = "scenario",
+        values_to = "production"
+    ) |>
+    dplyr::group_by_at(c("year", linecolor, facet_var)) |>
+    dplyr::mutate(
+        max_production = max(production, na.rm = TRUE),
+        production_pct = production / max_production * 100
+    ) |>
+    dplyr::ungroup()
+
+  return(data_trisk_line_plot)
+}
+
+
+draw_trisk_line_plot <- function(
+    data_trisk_line_plot,
+    x_var,
+    facet_var,
+    linecolor) {
+  facets_colors <- r2dii.colours::palette_2dii_plot[seq_along(unique(data_trisk_line_plot[[linecolor]])), ]$hex
+trisk_line_plot <- ggplot(
+        data_trisk_line_plot, 
+        aes(
+            x = !!rlang::sym(x_var), 
+            y = production_pct, 
+            color = !!rlang::sym(linecolor),
+            linetype = scenario)
+    ) +
+    ggplot2::geom_line() +
+    ggplot2::geom_point(size = 0.5) +
+    ggplot2::scale_y_continuous(labels = scales::percent_format(scale=1)) +
+    ggplot2::scale_linetype_manual(values = c(
+        "production_baseline_scenario" = "dashed", 
+        "production_target_scenario" = "dotted", 
+        "production_shock_scenario" = "solid")) +
+    ggplot2::labs(
+        x = "Year",
+        y = "Production as a percentage of the maximum",
+        linetype = "Scenario"
+    ) +
+    ggplot2::scale_color_manual(values = facets_colors) +
+    r2dii.plot::theme_2dii() +
+    ggplot2::theme(panel.grid.major.y = ggplot2::element_line(size = .1, color = "black")) +
+    ggplot2::facet_wrap(stats::as.formula(paste("~", paste(facet_var, collapse = "+"))), scales = "fixed", ncol = 2) +
+    ggplot2::expand_limits(y = 0) 
+
+  return(trisk_line_plot)
+}
--- app/view/modules/plots_trajectories.R
+++ app/view/modules/plots_trajectories.R
@@ -1,10 +1,9 @@
 box::use(
-  shiny[moduleServer, NS, req, tagList, div, plotOutput, uiOutput, eventReactive, observeEvent, renderUI, renderPlot, reactive],
-  semantic.dashboard[box]
+  shiny[moduleServer, NS, tagList, plotOutput, uiOutput, observeEvent, renderPlot]
 )
 
 box::use(
-  app/logic/plots/scenario_time_plot[pipeline_scenario_time_plot]
+  app/logic/plots/trisk_lineplot[pipeline_trisk_line_plot]
 )
 
 
@@ -13,11 +12,8 @@ box::use(
 ui <- function(id) {
   ns <- NS(id)
   tagList(
-    # Third row with 1 taking the entire page width
-    box(title = "Baseline Trajectories", width = 16, plotOutput(ns("baseline_scenario_plot"), height = "900px")),
+    semantic.dashboard::box(title = "Production Trajectories", width = 16, plotOutput(ns("trisk_line_plot_output"), height = "900px")),
 
-    # Fourth row with 2 taking the entire page width
-    box(title = "Shock Trajectories", width = 16, plotOutput(ns("shock_scenario_plot"), height = "900px"))
   )
 }
 
@@ -31,35 +27,16 @@ server <- function(id, trajectories_data_r, max_trisk_granularity) {
       granul_levels <- dplyr::intersect(colnames(trajectories_data_r()), names(max_trisk_granularity))
       granul_top_level <- names(max_trisk_granularity[granul_levels])[which.max(unlist(max_trisk_granularity[granul_levels]))]
 
-      ### BASELINE SCENARIO
-
       # Render plot
-      scenario_time_plot_baseline <- pipeline_scenario_time_plot(trajectories_data_r(),
-        y_var = "production_baseline_scenario",
-        linecolor = "ald_sector",
+      trisk_line_plot <- pipeline_trisk_line_plot(
+        trajectories_data=trajectories_data_r(),
         facet_var = granul_top_level
       )
 
-      output$baseline_scenario_plot <- renderPlot({
-        scenario_time_plot_baseline +
-          ggplot2::labs(title = "Production trajectories for the Baseline scenario")
+      output$trisk_line_plot_output <- renderPlot({
+        trisk_line_plot
       })
     })
-    observeEvent(trajectories_data_r(), ignoreInit = TRUE, {
-      granul_levels <- dplyr::intersect(colnames(trajectories_data_r()), names(max_trisk_granularity))
-      granul_top_level <- names(max_trisk_granularity[granul_levels])[which.max(unlist(max_trisk_granularity[granul_levels]))]
 
-      ### SHOCK SCENARIO
-      # Render plot
-      scenario_time_plot_shock <- pipeline_scenario_time_plot(trajectories_data_r(),
-        y_var = "production_shock_scenario",
-        linecolor = "ald_sector",
-        facet_var = granul_top_level
-      )
-      output$shock_scenario_plot <- renderPlot({
-        scenario_time_plot_shock +
-          ggplot2::labs(title = "Production trajectories for the Shock scenario")
-      })
-    })
   })
 }
