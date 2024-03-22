6c00ac87c35fde87a85c870b6fd82b304b0fc20c
bertrand <bertrand.gallice.pro@gmail.com>
2024-03-22 23:54:09 +0100

    wip

--- scripts/shinyproxy/deploy.sh
+++ scripts/shinyproxy/deploy.sh
@@ -6,11 +6,12 @@ if [ "$#" -ne 3 ]; then
     exit 1
 fi
 
-MY_REGISTRY_URL=$(echo -n "${1}" | base64)
-MY_REGISTRY_USERNAME=$(echo -n "${2}" | base64)
-MY_REGISTRY_PASSWORD=$(echo -n "${3}" | base64)
+MY_REGISTRY_URL=$(echo -n "$1" | base64)
+MY_REGISTRY_USERNAME=$(echo -n "$2" | base64)
+MY_REGISTRY_PASSWORD=$(echo -n "$3" | base64)
 
 # Substitute variables in k8s.yaml and apply
 kubectl apply -f <(sed -e "s|\${MY_REGISTRY_URL}|${MY_REGISTRY_URL}|g" \
-    -e "s|\${MY_REGISTRY_USERNAME}|${MY_REGISTRY_URL}|g" \
-    -e "s|\${MY_REGISTRY_PASSWORD}|${MY_REGISTRY_URL}|g" k8s.yaml)
+    -e "s|\${MY_REGISTRY_USERNAME}|${MY_REGISTRY_USERNAME}|g" \
+    -e "s|\${MY_REGISTRY_PASSWORD}|${MY_REGISTRY_PASSWORD}|g" k8s.yaml)
+
